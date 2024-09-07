13d8a6dba8e3ae40af0d364c5003ae321283e703
AniD-z <137194673+AniD-z@users.noreply.github.com>
2024-09-07 15:54:41 +0530

    css

--- app/components/Billing.tsx
+++ app/components/Billing.tsx
@@ -7,41 +7,44 @@ import { motion } from "framer-motion";
 const Billing = () => (
   <section id="product" className="sectionReverse">
     <motion.div
-      className="sectionImgReverse"
-      variants={slideIn("left", "tween", 0.2, 1.5)}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: true }}
-    >
-      <Image
-        src={bill}
-        alt="billing"
-        width={0}
-        height={0}
-        className="w-[100%] h-[100%] relative z-[5]"
-      />
+  className="sectionImgReverse"
+  variants={slideIn("left", "tween", 0.1, 0.5)}
+  initial="hidden"
+  whileInView="show"
+  viewport={{ once: true }}
+>
+  <Image
+    src={bill}
+    alt="billing"
+    layout="responsive"
+    width={800}
+    height={600}
+    className="w-[100%] h-[100%] relative z-[5]"
+  />
 
-      {/* gradient start */}
-      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
-      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
-      {/* gradient end */}
-    </motion.div>
+  {/* gradient start */}
+  <div className="absolute z-[3] -left-1/2 top-0 w-[40%] h-[40%] rounded-full white__gradient" />
+  <div className="absolute z-[0] w-[40%] h-[40%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+  {/* gradient end */}
+</motion.div>
 
-    <motion.div
-      className="sectionInfo"
-      variants={slideIn("right", "tween", 0.2, 1.5)}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: true }}
-    >
-      <Image
-        src={bill}
-        alt="billing"
-        width={0}
-        height={0}
-        className="w-[100%] h-[100%] relative z-[5]"
-      />
-    </motion.div>
+<motion.div
+  className="sectionInfo"
+  variants={slideIn("right", "tween", 0.1, 0.5)}
+  initial="hidden"
+  whileInView="show"
+  viewport={{ once: true }}
+>
+  <Image
+    src={bill}
+    alt="billing"
+    layout="responsive"
+    width={800}
+    height={600}
+    className="w-[100%] h-[100%] relative z-[5]"
+  />
+</motion.div>
+
   </section>
 );
 
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -9,50 +9,51 @@ const Hero = () => {
   return (
     <section id="home" className="flex md:flex-row flex-col paddingY">
       <motion.div
-        className="flex-1 flexStart flex-col xl:px-0 paddingX"
-        variants={slideIn("left", "tween", 0.5, 1.5)}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: true }}
-      >
-        <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-          Threaded  <br className="sm:block hidden" />{" "}
-            <span className="text-gradient">Sustainably </span>{" "}
-          </h1>
-          <div className="ss:flex hidden md:mr-4 mr-0">
-            <GetStarted />
-          </div>
-        </div>
-        <p className="paragraph max-w-[470px] mt-5">
-        Our team of experts uses a methodology to identify the finest fabrics that are most likely to meet your
-        customers' needs. We examine thread counts, fabric weights, and weave densities to ensure the exceptional quality
-        of our premium cloths, which we supply to other brands.
-        </p>
-      </motion.div>
+  className="flex-1 flexStart flex-col xl:px-0 paddingX"
+  variants={slideIn("left", "tween", 0.1, 0.5)}
+  initial="hidden"
+  whileInView="show"
+  viewport={{ once: true }}
+>
+  <div className="flex flex-row justify-between items-center w-full">
+    <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+      Threaded  <br className="sm:block hidden" />{" "}
+      <span className="text-gradient">Sustainably </span>{" "}
+    </h1>
+    <div className="ss:flex hidden md:mr-4 mr-0">
+      <GetStarted />
+    </div>
+  </div>
+  <p className="paragraph max-w-[470px] mt-5">
+    Our team of experts uses a methodology to identify the finest fabrics that are most likely to meet your
+    customers' needs. We examine thread counts, fabric weights, and weave densities to ensure the exceptional quality
+    of our premium cloths, which we supply to other brands.
+  </p>
+</motion.div>
 
-      <motion.div
-        className="flex-1 flexCenter md:my-0 my-10 relative"
-        variants={slideIn("right", "tween", 0.2, 1)}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: true }}
-      >
-        <Image
-          src={robot}
-          alt="billing"
-          width={0}
-          height={0}
-          className="sm:w-[100%] w-[90%] sm:h-[100%] h-[90%] relative z-[5]"
-          priority={true}
-        />
+<motion.div
+  className="flex-1 flexCenter md:my-0 my-10 relative"
+  variants={slideIn("right", "tween", 0.1, 0.5)}
+  initial="hidden"
+  whileInView="show"
+  viewport={{ once: true }}
+>
+  <Image
+    src={robot}
+    alt="billing"
+    width={0}
+    height={0}
+    className="sm:w-[100%] w-[90%] sm:h-[100%] h-[90%] relative z-[5]"
+    priority={true}
+  />
 
-        {/* gradient start */}
-        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
-        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-        {/* gradient end */}
-      </motion.div>
+  {/* gradient start */}
+  <div className="absolute z-[0] w-[30%] h-[30%] top-0 pink__gradient" />
+  <div className="absolute z-[1] w-[60%] h-[60%] rounded-full white__gradient bottom-40" />
+  <div className="absolute z-[0] w-[40%] h-[40%] right-20 bottom-20 blue__gradient" />
+  {/* gradient end */}
+</motion.div>
+
 
       <div className="ss:hidden flexCenter">
         <GetStarted />
